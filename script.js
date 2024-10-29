ec74cbe57b27da2075e3e976b3c419c0b71a882a
PaladinArthas <battlelog18@mail.ru>
2024-10-29 20:31:31 +0500

    Gleb
    123

---
+++ script.js
@@ -0,0 +1,183 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function showModal1() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 13) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal2() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3241) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal3() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal4() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == "1360") {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal5() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == "0,25") {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal6() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal7() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 13) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal8() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3241) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal9() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal10() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 13) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal11() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3241) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal12() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal13() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 13) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal14() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3241) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal15() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal16() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 13) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal17() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3241) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal18() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal19() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 13) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal20() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3241) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
+function showModal21() {
+  let name = prompt("Ваш ответ?", "");
+  if (name == 3) {
+    alert("Правильно!");
+  } else {
+    alert("Неправильно, решите ещё раз");
+  }
+}
